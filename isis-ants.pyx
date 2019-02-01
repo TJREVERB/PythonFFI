8133816acf5be4188d50065d50272882f76baf44
max2442 <43480994+max2442@users.noreply.github.com>
2019-02-01 13:21:21 -0500

    Update isis-ants.pyx

--- isis-ants.pyx
+++ isis-ants.pyx
@@ -18,5 +18,39 @@ cdef extern from "ants-api.h":
     KANTSStatus k_ants_watchdog_stop(void)
     KANTSStatus k_ants_passthrough(const uint8_t * tx, int tx_len, uint8_t * rx,int rx_len)
 
-def py_hello(name: bytes) -> None:
-    hello(name)
+def py_k_ants_init(bus: bytes,primary: bytes,secondary: bytes,ant_count: bytes,timeout: bytes) -> None:
+    k_ants_init(bus,primary,secondary,ant_count,timeout)
+def py_k_ants_terminate(void) -> None:
+    k_ants_terminate()
+def py_k_ants_configure(config: bytes) -> None:
+    k_ants_configure(config)
+def py_k_ants_reset(void) -> None:
+    k_ants_reset()
+def py_k_ants_arm(void) -> None:
+    k_ants_arm()
+def py_k_ants_disarm(void) -> None:
+    k_ants_disarm()
+def py_k_ants_deploy(antenna: bytes,override: bytes,timeout: bytes) -> None:
+    k_ants_deploy(antenna,override,timeout)
+def py_k_ants_auto_deploy(timeout: bytes) -> None:
+    k_ants_auto_deploy(timeout)
+def py_k_ants_cancel_deploy(void) -> None:
+    k_ants_cancel_deploy()
+def py_k_ants_get_deploy_status(resp: bytes) -> None:
+    k_ants_get_deploy_status(bytes)
+def py_k_ants_get_uptime(uptime: bytes) -> None:
+    k_ants_get_uptime(uptime)
+def py_k_ants_get_system_telemetry(telem: bytes) -> None:
+    k_ants_get_system_telemetry(telem)
+def py_k_ants_get_activation_count(antenna: bytes,count: bytes) -> None:
+    k_ants_get_activation_count(antenna,count)
+def py_k_ants_get_activation_time(antenna: bytes,time: bytes) -> None:
+    k_ants_get_activation_time(antenna,time)
+def py_k_ants_watchdog_kick(void) -> None:
+    k_ants_watchdog_kick()
+def py_k_ants_watchdog_start(void) -> None:
+    k_ants_watchdog_start()
+def py_k_ants_watchdog_stop(void) -> None:
+    k_ants_watchdog_stop()
+def py_k_ants_passthrough(tx: bytes,tx_len: bytes,rx: bytes,rx_len: bytes) -> None:
+    k_ants_passthrough(tx,tx_len,rx,rx_len)
