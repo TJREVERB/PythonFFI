80bb3259794723b7df2f76a620035373a01283cb
max2442 <43480994+max2442@users.noreply.github.com>
2019-02-03 13:41:35 -0500

    Update isisants.pyx

--- isisants.pyx
+++ isisants.pyx
@@ -1,9 +1,20 @@
 import stdint
 import stdbool
-typedef enum{ANT_1, ANT_2, ANT_3, ANT_4} KANTSAnt;
-typedef struct{uint16_t raw_temp; uint16_t deploy_status; uint32_t uptime;} __attribute__((packed)) ants_telemetry;
-typedef enum{ANTS_OK, ANTS_ERROR, ANTS_ERROR_CONFIG, ANTS_ERROR_NOT_IMPLEMENTED} KANTSStatus;
 cdef extern from "ants-api.h":
+    ctypedef enum KANTSAnt:
+        ANT_1, 
+        ANT_2, 
+        ANT_3, 
+        ANT_4
+    ctypedef struct __attribute__((packed)) ants_telemetry:
+        uint16_t raw_temp; 
+        uint16_t deploy_status; 
+        uint32_t uptime
+    ctypedef enum KANTSStatus:
+        ANTS_OK, 
+        ANTS_ERROR, 
+        ANTS_ERROR_CONFIG, 
+        ANTS_ERROR_NOT_IMPLEMENTED
     KANTSStatus k_ants_init(char * bus, uint8_t primary, uint8_t secondary, uint8_t ant_count, uint32_t timeout)
     void k_ants_terminate()
     KANTSStatus k_ants_configure(KANTSController config)
