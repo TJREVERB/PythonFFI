78ec9f7b552699bb78ac82c726413b58b56c0642
max2442 <43480994+max2442@users.noreply.github.com>
2019-02-04 21:05:27 -0500

    Update Convert.c

--- Convert.c
+++ Convert.c
@@ -4,7 +4,7 @@ unsigned short convert16(uint16_t p)
     unsigned short q = p;
     return q;
 }
-unsigned short convert16(uint32_t r)
+unsigned short convert32(uint32_t r)
 {
     unsigned int k = r;
     return k;
