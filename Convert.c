da193fd7a73801f5e1c0bcb5849f13d415112635
max2442 <43480994+max2442@users.noreply.github.com>
2019-02-04 21:17:52 -0500

    Update Convert.c

--- Convert.c
+++ Convert.c
@@ -1,10 +1,10 @@
 #include <stdint.h>
-unsigned short convert16(uint16_t p)
+unsigned short convert16(uint16_t * p)
 {
     unsigned short q = p;
     return q;
 }
-unsigned short convert32(uint32_t r)
+unsigned short convert32(uint32_t * r)
 {
     unsigned int k = r;
     return k;
