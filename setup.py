6ebd22861187054bf031d7e0e13a2473664f8e93
max2442 <43480994+max2442@users.noreply.github.com>
2019-02-06 15:18:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
 from distutils.core import setup
-from distutils.extension import Extension
 from Cython.Build import cythonize
 
-examples_extension = Extension(
-    name="isisants",
-    sources=["isisants.pyx"],
-    libraries=["examples"],
-)
 setup(
-    name="isisants",
-    ext_modules=cythonize([examples_extension])
+    ext_modules = cythonize("isisants.pyx")
 )
