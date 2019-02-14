5d99e8c4aacfe099a730edac455d7069542b6d0b
max2442 <43480994+max2442@users.noreply.github.com>
2019-02-14 16:55:52 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from Cython.Build import cythonize
 examples_extension = Extension(
     name="isisants",
     sources=["isisants.pyx"],
-    libraries=["ants"],
+    libraries=["ants", "n"],
     library_dirs=["lib"],
     include_dirs=["lib"]
 )
