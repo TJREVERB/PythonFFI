2ad42372cdbc9fbffdfa6c4ba15ecf4eed8e015a
max2442 <43480994+max2442@users.noreply.github.com>
2019-03-01 13:24:07 -0500

    Delete deploystatus.h

--- lib/deploystatus.h
+++
@@ -1,7 +0,0 @@
-typedef enum {
-    ANTS_OK,                     /**< Requested function completed successfully */
-    ANTS_ERROR,                  /**< Generic error */
-    ANTS_ERROR_CONFIG,           /**< Configuration error */
-    ANTS_ERROR_NOT_IMPLEMENTED   /**< Requested function has not been implemented for the subsystem */
-} KANTSStatus;
-KANTSStatus p_k_ants_get_deploy_status();
