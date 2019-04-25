19807623782e9510ed23a666e97d8b2e8ce908ab
max2442 <43480994+max2442@users.noreply.github.com>
2019-04-25 14:15:23 -0400

    Update and rename main.py to isisants.py

---
+++ isisants.py
@@ -0,0 +1,12 @@
+import isisants
+isisants.py_k_ants_init(b"/dev/i2c-1",0x31,0x32,4,10)
+isisanst.py_k_ants_arm()
+def deploy():
+    ANT_1 = 0
+    ANT_2 = 1
+    ANT_3 = 2
+    ANT_4 = 3
+    isisants.py_k_ants_deploy(ANT_1, False, 5)
+    isisants.py_k_ants_deploy(ANT_2, False, 5)
+    isisants.py_k_ants_deploy(ANT_3, False, 5)
+    isisants.py_k_ants_deploy(ANT_4, False, 5)
