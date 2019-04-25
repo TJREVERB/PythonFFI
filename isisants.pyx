90fadd9ac521df265c7fb6045c91e472b7484a52
max2442 <43480994+max2442@users.noreply.github.com>
2019-04-25 18:19:41 -0400

    Update isisants.pyx

--- isisants.pyx
+++ isisants.pyx
@@ -59,7 +59,7 @@ def py_k_ants_init(char * bus, uint8_t primary, uint8_t secondary, uint8_t ant_c
 def py_k_ants_terminate():
     k_ants_terminate()
 def py_k_ants_configure(config):
-    return k_ants_configure(KANTSController config)
+    return k_ants_configure(<KANTSController>config)
 def py_k_ants_reset():
     return k_ants_reset()
 def py_k_ants_arm():
@@ -67,9 +67,9 @@ def py_k_ants_arm():
 def py_k_ants_disarm():
     return k_ants_disarm()
 def py_k_ants_deploy(antenna, override, timeout):
-    return k_ants_deploy(KANTSAnt antenna,bool override,uint8_t timeout)
+    return k_ants_deploy(<KANTSAnt>antenna,<bool>override,<uint8_t>timeout)
 def py_k_ants_auto_deploy(timeout):
-    return k_ants_auto_deploy(uint8_t timeout)
+    return k_ants_auto_deploy(<uint8_t>timeout)
 def py_k_ants_cancel_deploy():
     return k_ants_cancel_deploy()
 def py_k_ants_get_deploy_status(resp):
@@ -79,9 +79,9 @@ def py_k_ants_get_uptime(uptime):
 def py_k_ants_get_system_telemetry(telem):
     return k_ants_get_system_telemetry(<ants_telemetry *>telem)
 def py_k_ants_get_activation_count(antenna, count):
-    return k_ants_get_activation_count(KANTSAnt antenna,uint8_t * count)
+    return k_ants_get_activation_count(<KANTSAnt>antenna,<uint8_t *>count)
 def py_k_ants_get_activation_time(antenna, time):
-    return k_ants_get_activation_time(KANTSAnt antenna,<uint16_t *>time)
+    return k_ants_get_activation_time(<KANTSAnt>antenna,<uint16_t *>time)
 def py_k_ants_watchdog_kick():
     return k_ants_watchdog_kick()
 def py_k_ants_watchdog_start():
@@ -89,12 +89,12 @@ def py_k_ants_watchdog_start():
 def py_k_ants_watchdog_stop():
     return k_ants_watchdog_stop()
 def py_k_ants_passthrough(tx, tx_len,rx,rx_len):
-    return k_ants_passthrough(const uint8_t * tx,int tx_len,uint8_t * rx,int rx_len)
+    return k_ants_passthrough(<const uint8_t *>tx,<int>tx_len,<uint8_t *>rx,<int>rx_len)
 def py_k_i2c_init(device, fp):
-    return k_i2c_init(char * device, <int *>fp)
+    return k_i2c_init(<char *>device, <int *>fp)
 def py_k_i2c_terminate(fp):
     k_i2c_terminate(<int *>fp)
 def py_k_i2c_write(i2c, addr, ptr, len):
-    return k_i2c_write(int i2c, <uint16_t>addr, uint8_t * ptr, int len)
+    return k_i2c_write(<int>i2c, <uint16_t>addr, <uint8_t *>ptr, <int>len)
 def py_k_i2c_read( i2c, addr, ptr, int len):
-    return k_i2c_read(int i2c, <uint16_t>addr, uint8_t * ptr, int len)
+    return k_i2c_read(<int>i2c, <uint16_t>addr, <uint8_t *>ptr, <int>len)
